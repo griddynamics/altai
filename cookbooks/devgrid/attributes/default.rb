55132f50c635da836e7a535e34e29760d426e1a6
Aleksei Kornev <aleksei.kornev@gmail.com>
2012-06-05 16:17:37 +0400

    Add recipe for adding epel and gd to rpm repo

---
+++ cookbooks/devgrid/attributes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: devgrid
+# 
+#
+# Copyright 2012, GridDynamics
+#
+# All rights reserved - Do Not Redistribute
+#
+
+default[:epel][:rpm_url] = "http://download.fedoraproject.org/pub/epel/6/i386/epel-release-6-5.noarch.rpm"
+default[:gd_openstack][:url] = "http://yum.griddynamics.net/yum/essex-centos/"
---
+++ cookbooks/devgrid/recipes/repos.rb
@@ -0,0 +1,29 @@
+#
+# Cookbook Name:: devgrid
+# Recipe:: default
+#
+# Copyright 2012, GridDynamics
+#
+# All rights reserved - Do Not Redistribute
+#
+
+case node["platform"]
+    when "centos", "redhat", "fedora" do
+
+        execute "add_epel_repository" do
+            command "rpm -Uhv #{node[:epel][:rpm_url]}"
+            not_if do
+                File.exists?("/etc/yum.repos.d/epel.repo") 
+            end
+        end
+
+        template "/etc/yum.repos.d/gd-openstack.repo" do
+            source "gd-openstack.erb"
+            mode 644
+            owner "root"
+            group "root"
+            not_if do
+                File.exists?("/etc/yum.repos.d/gd-openstack.repo")
+            end
+        end
+    end
