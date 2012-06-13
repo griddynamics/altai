9d1375ba6b959645cd2be2ea93a8c9b6f4903257
Aleksei Kornev <aleksei.kornev@gmail.com>
2012-06-13 15:12:53 +0400

    Add ability to install odb

---
+++ cookbooks/devgrid/recipes/odb.rb
@@ -0,0 +1,37 @@
+#
+# Cookbook Name:: devgrid
+# Recipe:: default
+#
+# Copyright 2012, GridDynamics
+#
+# All rights reserved - Do Not Redistribute
+#
+
+
+log("Start to install odb")
+
+package "mysql-server" do
+    action :install
+end
+
+log("Apply config with binding address"){level :debug}
+template "/etc/my.cnf" do
+    source "my.cnf.erb"
+    mode 644
+    owner "root"
+    group "root"
+end
+
+log("Start mysql service"){level :debug}
+service "mysqld" do
+    action :start
+end
+
+log("Setup root user"){level :debug}
+execute "setup_root_password" do
+    command "mysqladmin -u root password '#{node["mysql-root-password"]}'"
+end
+log("Odb was succesfully installed")
+
+
+
