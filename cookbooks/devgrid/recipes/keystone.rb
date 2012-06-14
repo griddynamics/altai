f68e543566a50a5f13e5b7309e96ae50378fabd9
Nikita Savin <nsavin@griddynamics.com>
2012-06-15 00:49:49 +0300

    * re-write keystone receipt

--- cookbooks/devgrid/recipes/keystone.rb
+++ cookbooks/devgrid/recipes/keystone.rb
@@ -13,7 +13,7 @@ require "uuid"
 log("Start to install keystone")
 node.set["mysql-keystone-password"] = UUID.new().generate()
 node.set["keystone-magic-token"] = UUID.new().generate()
-
+keystone_cmd = "keystone  --token=#{node["keystone-magic-token"]} --endpoint http://localhost:35357/v2.0"
 %w(openstack-keystone-essex python-keystoneclient-essex).each do |package_name|
     package package_name do 
         action :install
@@ -53,41 +53,30 @@ service "keystone" do
 end
 
 execute "sync_keystone" do
-    command "sleep 1"
+    command "sleep 2"
 end
 
-# I've commented it because as I understand all services should be configured in catalog.template file
-#log("Add services")
-#
-#[{:name => "nova", :type => "compute", :description => "OpenStack Compute Service"}
-#{:name => "nova-volume", :type => "volume", :description => "OpenStack Nova Volume Service"}
-#{:name => "glance", :type => "image", :description => "OpenStack Image Service"}
-#{:name => "keystone", :type => "identity", :description => "OpenStack Identity Service"}
-#{:name => "quantum", :type => "network", :description => "Openstack Network Service"}].each do |service|
-#    execute "Create service #{service[:name]}" do
-#        command "keystone service-create --name #{service[:name]} 
-#                    --type #{service[:type]} --description '#{service[:description]}'"
-#    end
-#end
-
-log("Add roles")
-%w(admin member).each do |role|
-    execute "Add role #{role}" do
-        command "keystone --token=#{node["keystone-magic-token"]} --endpoint http://localhost:35357/v2.0 --os_username=1 --os_password=1 role-create --name=#{role}"
-    end
-end
-
-log("Add admin tenant")
-execute "Add admin tenant" do
-    command "keystone  --token=#{node["keystone-magic-token"]} --endpoint http://localhost:35357/v2.0 --os_username=1 --os_password=1 tenant-create --name=systenant"
-end
-
-log("Add admin user")
-execute "Add admin user" do
-    command "keystone  --token=#{node["keystone-magic-token"]} --endpoint http://localhost:35357/v2.0 --os_username=1 --os_password=1 user-create --name='#{node["admin-login-name"]}' --tenant_id=systenant --pass='#{node["admin-login-password"]}' --email='#{node["admin-login-email"]}' --enabled true"
-end
-execute "Assign admin role" do
-    command "keystone  --token=#{node["keystone-magic-token"]} --endpoint http://localhost:35357/v2.0 --os_username=1 --os_password=1 user-role-add --user='#{node["admin-login-name"]}' --role=admin --tenant_name=systenant"
+log("Add admin, tenant and role")
+bash "Add admin tenant, user and role" do
+    environment ( {
+	'KCMD' => keystone_cmd, 
+	'LOGIN' => node["admin-login-name"],
+	'PASSWD' => node["admin-login-password"],
+	'EMAIL' => node["admin-login-email"] })
+    code <<-EOH
+    function get_id () { echo `$@ | awk '/ id / { print $4 }'`; }
+    echo "$KCMD, $LOGIN, $PASSWD"
+    ADMIN_ROLE=`get_id $KCMD role-create --name admin`
+    echo "Admin role: $ADMIN"
+    MEMBER_ROLE=`get_id $KCMD role-create --name member`
+    echo "Member role: $MEMBER"
+    TENANT=`get_id $KCMD tenant-create --name=systenant`
+    echo "Tenant id: $TENANT"
+    ADMIN=`get_id $KCMD user-create --name="$LOGIN" --tenant_id=systenant --pass='$PASSWD' --email='$EMAIL' --enabled true`
+    echo "admin id: $ADMIN"
+    $KCMD user-role-add --user="$ADMIN" --role="$ADMIN_ROLE" --tenant_id="$TENANT"
+    echo 5
+    EOH
 end
 
 log("Keystone was succesfully installed")
