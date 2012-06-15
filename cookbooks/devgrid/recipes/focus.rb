911129838e4619c23f1ed60e0c4aa0784c391905
Nikita Savin <nsavin@griddynamics.com>
2012-06-15 15:56:47 +0300

    focus receipt

--- cookbooks/devgrid/recipes/focus.rb
+++ cookbooks/devgrid/recipes/focus.rb
@@ -10,6 +10,36 @@
 
 log("Start to install focus")
 
+node.set["mysql-focus-password"] = UUID.new().generate()
+
+package "python-focus" do
+    action :install
+end
+
+mysql_create_database "focus" do
+end
+
+mysql_add_grants_for_user "focus" do
+    database :focus
+    password node["mysql-focus-password"]
+end
+
+template "/etc/focus/local_settings.py" do
+    source "focus/local_settings.py.erb"
+    mode 644
+    owner "root"
+    group "root"
+end
+
+execute "upload db" do 
+    command "mysql -u focus -p#{node['mysql-focus-password']} focus < /etc/focus/invitations_dump.sql"
+end
+
+log("Start services"){level :debug}
+service "focus" do
+    action [:enable, :start]
+end
+
 log("focus was succesfully installed")
 
 
