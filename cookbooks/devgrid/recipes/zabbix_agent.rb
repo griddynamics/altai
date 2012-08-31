b754d14e961d40ef05c2c7e025faf676ea2c7aae
Nikita Savin <nsavin@griddynamics.net>
2012-08-31 10:14:34 +0300

    * add zabbix_agent receipt

---
+++ cookbooks/devgrid/recipes/zabbix_agent.rb
@@ -0,0 +1,42 @@
+#    Altai Private Cloud 
+#    Copyright (C) GridDynamics Openstack Core Team, GridDynamics
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Lesser General Public License as published by
+#    the Free Software Foundation, either version 2.1 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+
+
+log("Start to install zabbix-agent")
+
+
+package "zabbix-agent"
+
+template "/etc/zabbix/zabbix_agent.conf" do
+    source "zabbix/zabbix_agent.conf.erb"
+    mode 00640
+    owner "root"
+    group "zabbix"
+end
+
+template "/etc/zabbix/zabbix_agentd.conf" do
+    source "zabbix/zabbix_agentd.conf.erb"
+    mode 00640
+    owner "root"
+    group "zabbix"
+end
+
+log("Start services"){level :debug}
+service "zabbix-agent" do 
+    action [:enable, :restart]
+end
+
+
+log("zabbix-agent was succesfully installed")
